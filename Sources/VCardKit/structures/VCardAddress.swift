1803008d194e5f819bdf615fe9b5d1fb72a2f480
Stephen Rudolph <Stephen.Rudolph@gmail.com>
2026-01-13 21:30:14 -0600

    Updating VCardAddress to include postalCode and de-emphasize PO Box and extended address per the RFC

--- Sources/VCardKit/structures/VCardAddress.swift
+++ Sources/VCardKit/structures/VCardAddress.swift
@@ -1,11 +1,18 @@
 import VComponentKit
 
 public struct VCardAddress: VPropertyEncodable {
+    /*
+     Per RFC 6350: "Experience with vCard 3 has shown that the first two components
+     (post office box and extended address) are plagued with many
+     interoperability issues.  To ensure maximal interoperability,
+     their values SHOULD be empty."
+     */
     public var postOfficeBox: String
     public var extendedAddress: String
     public var streetAddress: String
     public var locality: String // e.g. city
     public var region: String // e.g. state
+    public var postalCode: String
     public var country: String
 
     public var vEncoded: String {
@@ -13,18 +20,18 @@ public struct VCardAddress: VPropertyEncodable {
     }
 
     public init(
-        postOfficeBox: String = "",
-        extendedAddress: String = "",
         streetAddress: String = "",
         locality: String = "", // e.g. city
         region: String = "", // e.g. state
+        postalCode: String = "",
         country: String = ""
     ) {
-        self.postOfficeBox = postOfficeBox
-        self.extendedAddress = extendedAddress
+        self.postOfficeBox = ""
+        self.extendedAddress = ""
         self.streetAddress = streetAddress
         self.locality = locality
         self.region = region
+        self.postalCode = postalCode
         self.country = country
     }
 }
